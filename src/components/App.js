dc671c717413f946c0677af4f3b54804b000d895
darkdrew <andrewleong14@gmail.com>
2017-10-31 10:48:22 +0800

    changed url

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   }
 
   getAllPost = () => {
-    axios.get('https://mern-blogging-app.herokuapp.com/blogposts/posts')
+    axios.get('/blogposts/posts')
     .then( (response) => {
       this.setState({posts: response.data.results})
     }, (err) => {
