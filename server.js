12ea3f03b2cbb8be79c77ac618b4f3ad46c9bf3d
darkdrew <andrewleong14@gmail.com>
2017-11-01 11:10:14 +0800

    try

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ const router = require('./routes/blogRoutes');
 // ask app to use the routes for the API
 app.use('/blogposts', router);
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const port = process.env.PORT || 3001
 app.listen(port, () => {
